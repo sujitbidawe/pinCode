e6c3d8b9a95b33819741a104f3d78f4ac45ffedd
sujitbidawe <bidawesujit@gmail.com>
2020-03-27 01:09:54 +0530

    added: code to validate a six digit code

---
+++ pinCode.sh
@@ -0,0 +1,12 @@
+#!/usr/local/bin/bash
+
+read -p "enter a pin code to validate: " code
+
+#codePattern="^([a-zA-Z0-9]){6}$"
+codePattern="([0-9]){6}"
+if [[ $code =~ $codePattern ]]
+then
+	echo "$code is a valid postal code"
+else
+	echo "$code is not a valid postal code"
+fi
