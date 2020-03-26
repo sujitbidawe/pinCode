f693b66812d87b4a7c88bad28b9268b99b520ae7
sujitbidawe <bidawesujit@gmail.com>
2020-03-27 01:13:52 +0530

    added: code to restrict use of alphabets or special characters at the end

--- pinCode.sh
+++ pinCode.sh
@@ -3,7 +3,7 @@
 read -p "enter a pin code to validate: " code
 
 #codePattern="^([a-zA-Z0-9]){6}$"
-codePattern="^([0-9]){6}"
+codePattern="^([0-9]){6}$"
 if [[ $code =~ $codePattern ]]
 then
 	echo "$code is a valid postal code"
